713b9924741784340f9bde3f3d1b15d565afb8db
Jesus <fuertefernandez@gmail.com>
2021-01-29 19:38:53 +0100

    Salto de primera activity a segunda

--- app/src/main/java/com/example/pruebagit/MainActivity.kt
+++ app/src/main/java/com/example/pruebagit/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.pruebagit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun salto (view: View) {
+        val saltar = Intent(this, SegundaActivity::class.java)
+        this.startActivity(saltar)
     }
 }
